a23a026c007176870778840864422e6579960780
Abiyyu Nong Maulana Soge <149641808+AbiyyuNMS@users.noreply.github.com>
2026-01-12 00:52:30 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
 Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/{id}', [BookController::class, 'show']);
 Route::get('/category', [CategoryController::class, 'index']);
 
 Route::middleware('auth:api')->group(function () {
